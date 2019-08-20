876a61d2ab8cd71cc1a9bc28f24ccbd6382786aa
madmilla <madmilla@gmail.com>
2019-08-20 17:13:26 +0200

    ex20 & ex21

--- ex20.py
+++ ex20.py
@@ -13,6 +13,10 @@ def print_a_line(line_count, f):
 
 current_file = open(input_file)
 
+print("First let's print the whole file:\n")
+
+print_all(current_file)
+
 print("First let's rewind, kind of like a tape.")
 
 rewind(current_file)
@@ -20,5 +24,10 @@ rewind(current_file)
 print("Let's print three lines:")
 
 current_line = 1
-print_a_line(current_file)
-## TODO: finish this.
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
