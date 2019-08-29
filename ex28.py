42c3ce991f485f911b2fe97bc7f044ac7020910f
Madmilla <madmilla@gmail.com>
2019-08-29 13:59:59 +0200

    ex27 to ex32

---
+++ ex28.py
@@ -0,0 +1,95 @@
+# True and TRUE
+# = true
+#
+# false and true
+# = false
+#
+# 1 == 1 and 2 == 1
+# True and False
+# = False
+#
+# "test" == "test"
+# = true
+#
+# 1 == 1 or 2 != 1
+# True or TRUE
+# = true
+# 
+# True and 1 == 1
+# true and TRUE
+# = true
+#
+# false and 0 != 0
+# false and False
+# = false
+#
+# True or 1 == 1
+# true or TRUE
+# = true
+#
+# "test" == "testing"
+# = false
+#
+# 1 != 0 and 2 == 1
+# true and False
+# = False
+#
+# "test" != "testing"
+# = true
+#
+# "test" == 1
+# = false
+#
+# not (true and false)
+# true and false
+# = False
+# not (false)
+# = true
+#
+# not (1 == 1 and 0 != 1)
+# 1 == 1 = true
+# 0 != 1 = TRUE
+# true and true = TRUE
+# not (true) = false
+#
+# not (10 == 1 or 1000 == 1000)
+# 10 == 1 = False
+# 1000 == 1000 = TRUE
+# false or true = TRUE
+# not (true ) = false
+#
+# not (1 != 10 or 3 == 4)
+# 1 != 10 = TRUE
+# 3 == 4 = False
+# true or False = TRUE
+# not (true) = false
+#
+# not ("testing" == "testing" and "Zed" == "Cool guy")
+# testing == testing = TRUE
+# zed == cool guy = False
+# true and false = False
+# not false = TRUE
+#
+# 1 == 1 and (not ("testing" == 1 or 1 == 0))
+# 1 == 1 = true
+# testing == 1 = False
+# 1 == 0 = False
+# false or false = False
+# not false = true
+# true and true = TRUE
+#
+# "chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+# 3 == 4 = False
+# 3 == 3 = true
+# false or true = TRUE
+# not true = false
+# chunky == bacon = false
+# false and false = false
+#
+# 3 == 3 and (not ("testing" == "testing" or "python" == "fun"))
+# testing == testing = true
+# python == fun = False
+# true or false = TRUE
+# not (true) = False
+# 3 == 3 = TRUE
+# true and false = false
